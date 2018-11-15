899f9547bec33f7b220b0b6cbbb00b7c23cc1071
Andrew Reid East <andrew@andreweast.net>
2018-11-15 23:41:55 +0000

    Split into main file and module file

---
+++ main.py
@@ -0,0 +1,27 @@
+from readFile import parseCsv
+
+import tkinter as tk
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.read_file = tk.Button(self)
+        self.read_file["text"] = "Parse 'owls.csv'"
+        self.read_file["command"] = self.parse_csv
+        self.read_file.pack(side="top")
+
+        self.QUIT = tk.Button(self, text="QUIT", fg="red",
+                              command=root.destroy)
+        self.QUIT.pack(side="bottom")
+
+    def parse_csv(self):
+        parseCsv("owls.csv")
+
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
--- readFile.py
+++ readFile.py
@@ -1,6 +1,3 @@
-import tkinter as tk
-
-
 class Case():
     def __init__(self, csv_line, label_column):
         # self.num_attributes = num_attributes
@@ -49,26 +46,3 @@ def parseCsv(filename):
     print("Number of lines=" + str(count))
 
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
-
-    def createWidgets(self):
-        self.read_file = tk.Button(self)
-        self.read_file["text"] = "Parse 'owls.csv'"
-        self.read_file["command"] = self.parse_csv
-        self.read_file.pack(side="top")
-
-        self.QUIT = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
-        self.QUIT.pack(side="bottom")
-
-    def parse_csv(self):
-        parseCsv("owls.csv")
-
-
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
