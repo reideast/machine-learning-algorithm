d1db8ddaf177e432b17ef2801aacf271563923df
Jim <jamesfquaife@gmail.com>
2018-11-17 14:22:54 +0000

    Made a training method! Hope this is correct

---
+++ train.py
@@ -0,0 +1,13 @@
+from readFile import parseCsv
+
+class train():
+    def __init__(self):
+        self.tree=[]
+        self.numberOfLabels
+        
+        #This method is used for training
+        def _training(self):
+            for item in self.numberOfLabels:
+                self.tree[item].training();
+                
+  
