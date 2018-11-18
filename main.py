7028149d5456aa48530d08aeb1b33dc9f983a6dd
Andrew Reid East <andrew@andreweast.net>
2018-11-18 16:38:42 +0000

    Create input boxes to define column attributes

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from classes.Case import Case
 from train import train
 
 import tkinter as tk
-from tkinter import filedialog
-from tkinter import messagebox
-from tkinter import IntVar
+from tkinter import filedialog, messagebox, IntVar
 
 import logging
 import os
@@ -14,6 +12,7 @@ import os
 class Application(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
+        self.winfo_toplevel().title("Decision Tree Machine Learning")
         self.pack()
 
         self.filename = ""
@@ -21,11 +20,6 @@ class Application(tk.Frame):
         self.data_cases = None
         self.trainer = None
 
-        self.createWidgets()
-
-        self.winfo_toplevel().title("Decision Tree Machine Learning")
-
-    def createWidgets(self):
         self.frame_controls = tk.Frame(self)
         self.frame_controls.pack(side=tk.TOP)
 
@@ -41,6 +35,7 @@ class Application(tk.Frame):
 
         self.button_train = tk.Button(self.frame_controls)
         self.button_train["text"] = "Train on Data Set"
+        self.button_train["state"] = tk.DISABLED
         self.button_train["command"] = self.train_on_data
         self.image_train = tk.PhotoImage(file="images/process.png")
         self.button_train["compound"] = tk.LEFT
@@ -57,20 +52,22 @@ class Application(tk.Frame):
         self.button_previous.pack(pack_options_button)
 
         self.button_next = tk.Button(self.frame_controls)
-        self.button_next["text"] = "Next"
+        # self.button_next["text"] = "Next"
         # self.button_next["state"] = tk.DISABLED
         # self.button_next["command"] = lambda: messagebox.showinfo("Next", "Next")
-        self.button_next["command"] = lambda: self.show_subframe_columns()
+        self.button_next["text"] = "flip page"  # DEBUG
+        self.button_next["command"] = lambda: self.show_subframe_columns()  # DEBUG
         self.image_next = tk.PhotoImage(file="images/next.png")
         self.button_next["compound"] = tk.RIGHT
         self.button_next["image"] = self.image_next
         self.button_next.pack(pack_options_button)
 
         self.button_save = tk.Button(self.frame_controls)
-        self.button_save["text"] = "Save Results"
+        # self.button_save["text"] = "Save Results"
         # self.button_save["state"] = tk.DISABLED
         # self.button_save["command"] = lambda: messagebox.showinfo("Save", "Save")
-        self.button_save["command"] = lambda: self.show_subframe_tree()
+        self.button_save["text"] = "flip page 2"  # DEBUG
+        self.button_save["command"] = lambda: self.show_subframe_tree()  # DEBUG
         self.image_save = tk.PhotoImage(file="images/save.png")
         self.button_save["compound"] = tk.LEFT
         self.button_save["image"] = self.image_save
@@ -89,9 +86,6 @@ class Application(tk.Frame):
         self.tree_canvas.pack()
         self.tree_canvas.create_rectangle(0, 0, 600, 600, fill="white")
 
-        # self.subframe_columns = SubframeColumnSelector(self.frame_bottom, self)
-        # self.subframe_columns.grid(row=0, column=0, stick="nsew")
-
         self.subframe_columns = tk.Frame(self.frame_bottom)
         self.subframe_columns.grid(row=0, column=0, stick="nsew")
 
@@ -100,16 +94,23 @@ class Application(tk.Frame):
         self.subframe_col_options_inner = None
 
         self.button_process_csv = tk.Button(self.subframe_columns)
-        self.button_process_csv["text"] = "Process All Rows"
-        self.button_process_csv["command"] = lambda: messagebox.showinfo("process csv", "process csv")
+        self.button_process_csv["text"] = "Read Whole CSV File"
+        self.button_process_csv["command"] = self.save_file_attributes
         self.image_process_csv = tk.PhotoImage(file="images/data.png")
         self.button_process_csv["compound"] = tk.LEFT
         self.button_process_csv["image"] = self.image_process_csv
         pack_options_button["side"] = tk.TOP
         self.button_process_csv.pack(pack_options_button)
 
+        self.label_processed_csv = tk.Label(self.subframe_columns,
+                                            text="Data set loaded",
+                                            font="TkDefaultFont 8 italic")
+        # Not packed, so it is hidden
+
         self.add_col_options()  # Add default options, no data
         self.show_subframe_columns()
+
+    # ##################   Methods to support switching pages   ################### #
 
     def show_subframe_columns(self):
         self.hide_subframe_tree()
@@ -141,7 +142,7 @@ class Application(tk.Frame):
     def hide_subframe_tree(self):
         pass  # todo: need to disable anything for the tree canvas page?
 
-    # add a number of text/check boxes to select the columns' labels
+    # Add variable number of text/check boxes to input column labels
     def add_col_options(self):
         if self.subframe_col_options_inner is not None:
             self.subframe_col_options_inner.destroy()
@@ -162,7 +163,6 @@ class Application(tk.Frame):
         print()
 
         parent = self.subframe_col_options_inner
-        # TODO: need still? pack_options_text = {"side": tk.LEFT}  # , "padx": 8, "pady": 8, "ipadx": 4, "ipady": 4}
         self.cols_labels = []
         self.cols_text_boxes = []
         self.cols_radio_buttons = []
@@ -173,41 +173,71 @@ class Application(tk.Frame):
             self.cols_labels.append(label)
         for idx in range(num_cols):
             text_box = tk.Entry(parent, width=15)
-            # todo: note: text_box.get() should get the text contents
-            # text_box.pack(pack_options_text)
             text_box.grid(row=1, column=idx)
             self.cols_text_boxes.append(text_box)
         for idx in range(num_cols):
             radio = tk.Radiobutton(parent, text="Label", variable=self.cols_radio_var, value=idx)
-            # todo: note: self.cols_radio_var.get() should return an integer, e.g. value=idx
             radio.grid(row=2, column=idx)
             self.cols_radio_buttons.append(radio)
+        self.cols_radio_buttons[num_cols - 1].select()  # Select last in list, since many data sets have the final column as the label
 
+        # DEBUG:
+        if DEBUG and "owls.csv" in self.filename:  # also guards against filename not being set yet
+            for idx, name in enumerate(["body-length", "wing-length", "body-width", "wing-width", "type"]):
+                self.cols_text_boxes[idx].insert(0, name)
+
+    # ##################   Methods called by buttons to do main functionality   ################## #
     def load_file(self):
         chosen_file = filedialog.askopenfilename(initialdir=os.getcwd(),
-                                                   title="Choose a data file",
-                                                   filetypes=(("CSV files", "*.csv"), ("All files", "*.*")))
+                                                 title="Choose a data file",
+                                                 filetypes=(("CSV files", "*.csv"), ("All files", "*.*")))
         if chosen_file is not "":
             self.filename = chosen_file  # Temp variable used so cancelling the dialog when a file had already been loaded will not prevent proceeding
 
             self.is_file_prepared = False
+            self.label_processed_csv.pack_forget()  # Essentially, hide
+            Case.label_column = -1
+            self.button_train["state"] = tk.DISABLED
             logging.debug("Preparing to choose attributes for data file: " + self.filename)
 
-            # todo: show attribute picker GUI
+            # Recreate column attribute picker GUI elements for user to provide column names
             self.add_col_options()
             self.show_subframe_columns()
 
+    # Gather properties of CSV file from user-inputted GUI elements
     def save_file_attributes(self):
         logging.debug("Loading data file: " + self.filename)
-        self.data_cases = parse_csv(self.filename)
 
-        # TOOD: NOTE: copy list w/ objects:
+        if self.filename is not "":
+            Case.attributes_names = []
+            Case.label_column = self.cols_radio_var.get()
+            for idx, text_box in enumerate(self.cols_text_boxes):
+                value = text_box.get()
+                if len(value) == 0:
+                    value = "column" + str(idx + 1)
+                if idx == Case.label_column:
+                    Case.label_name = value
+                else:
+                    Case.attributes_names.append(value)
+
+            if DEBUG:
+                print("Metadata for Case: (label col num=" + str(Case.label_column) + ")")
+                print(", ".join(Case.attributes_names) + ", label=" + Case.label_name)
+
+            self.data_cases = parse_csv(self.filename)
+            self.label_processed_csv.pack()
+            self.button_train["state"] = tk.NORMAL
+            self.is_file_prepared = True
+        else:
+            messagebox.showwarning("No file loaded", "Cannot select file attributes: no data file has been loaded")
+
+        # TODO: NOTE: copy list w/ objects:
         # import copy
         # new_list = copy.deepcopy(old_list)
-        # overridding deepcopy mechanism for classes: __deepcopy__(self): https://docs.python.org/2/library/copy.html
+        # overriding deepcopy mechanism for classes: __deepcopy__(self): https://docs.python.org/2/library/copy.html
 
     def train_on_data(self):
-        if self.filename is not "":
+        if self.data_cases is not "":  # TODO: rename to self.master_data_set
             self.trainer = train(self.data_cases)
         else:
             messagebox.showwarning("No file loaded", "Cannot train model: no data file has been loaded")
