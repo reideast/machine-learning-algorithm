81a705909d12463dc1d1a9b14a0d465ca1b1e1d3
Andrew Reid East <andrew@andreweast.net>
2018-11-18 17:57:26 +0000

    Remove unneeded URLs from About

--- about.py
+++ about.py
@@ -9,17 +9,11 @@ Computer Science CT475: Machine Learning
 November 2018 
 Supervisor: Dr. Michael Madden
 
-
 Image Attribution:
-process by popcornarts from the Noun Project
-https://thenounproject.com/search/?q=process&i=1056444
-open by Landan Lloyd from the Noun Project
-https://thenounproject.com/search/?q=open&i=1509580
-previous by Three Six Five from the Noun Project
-https://thenounproject.com/search/?q=previous&i=1708821
+Open by Landan Lloyd from the Noun Project
+Process by popcornarts from the Noun Project
+Previous by Three Six Five from the Noun Project
 Next by Three Six Five from the Noun Project
-https://thenounproject.com/365/uploads/?i=1708819
 Save by Sophia Bai from the Noun Project
-https://thenounproject.com/search/?q=save&i=1919373
-Data by BomSymbols from the Noun Project
-https://thenounproject.com/term/data/610617/"""
+Info by ProSymbols from the Noun Project
+Data by BomSymbols from the Noun Project"""
